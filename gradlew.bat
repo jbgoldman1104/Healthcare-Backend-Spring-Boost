687d1fc6d4ec166918dc34e825adbaedc0a12ec5
jbgoldman1104 <jbgoldman1104@gmail.com>
2023-07-27 15:34:15 +0900

    "updated on 2023-07-27"

--- gradlew.bat
+++ gradlew.bat
@@ -8,6 +8,17 @@
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem
+
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
